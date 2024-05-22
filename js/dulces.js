da84c8ed73bac8c5ae35df423768c2ecc7fc4fee
Juan Pablo Nieto <juanpablonieto123@gmail.com>
2024-05-21 22:52:04 -0300

    Título de páginas y favicon agregados

--- js/dulces.js
+++ js/dulces.js
@@ -64,11 +64,13 @@
 // }
 
 
+// Referenciar contenido de páginas según el parametro focus
 window.onload = function() {
     console.log("El script se ha cargado correctamente.");
-
+    // Obtener los parámetros de la URL
     const urlParams = new URLSearchParams(window.location.search);
     const focus = urlParams.get('focus');
+
     console.log("Parámetro focus:", focus);
 
     // Muestra solo el contenedor relevante basado en el parámetro 'focus'
@@ -83,3 +85,37 @@ window.onload = function() {
         }
     }
 };
+
+
+// Para cambiar el título de la página
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener los parámetros de la URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const focus = urlParams.get('focus');
+
+    // Comprobar el valor del parámetro 'focus' y cambiar el título de la página
+    if (focus) {
+        switch(focus) {
+            case 'bizcochuelo':
+                document.title = "Bizcochuelo de Chocolate - Dulce Capricho";
+                break;
+            case 'cheesecake':
+                document.title = "Cheesecake de Dulce de Leche - Dulce Capricho";
+                break;
+            case 'lemonpie':
+                document.title = "Lemon Pie Tradicional - Dulce Capricho";
+                break;
+            case 'trufas':
+                document.title = "Trufas de Chocolate - Dulce Capricho";
+                break;
+            case 'cupcake':
+                document.title = "Cupcake de Vainilla - Dulce Capricho";
+                break;
+            case 'alfajor':
+                document.title = "Alfajores de Maicena - Dulce Capricho";
+                break;
+            default:
+                document.title = "Dulce Capricho";
+        }
+    }
+});
